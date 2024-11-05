cbb1fd599285e5cab841e2d2180320eb65405af4
Noe Rabinovici <norabino@c2r4p7.42perpignan.fr>
2024-11-05 14:04:39 +0100

    auto update

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: norabino <norabino@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 10:05:28 by norabino          #+#    #+#             */
-/*   Updated: 2024/11/05 13:58:24 by norabino         ###   ########.fr       */
+/*   Updated: 2024/11/05 14:04:36 by norabino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void *ft_memcpy(void *dest, const void *src, size_t n)
     size_t  i;
 
     i = 0;
-    if (src == 0 || n == 0)
+    if (src == 0 || n != 0)
     {
         if (dest == 0)
             return (NULL);
