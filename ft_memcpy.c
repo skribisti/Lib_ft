12170671b6ef0b8e7550b5ecd61cb4a8c32efe3e
Noe Rabinovici <norabino@c2r4p7.42perpignan.fr>
2024-11-05 14:12:55 +0100

    auto update

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: norabino <norabino@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 10:05:28 by norabino          #+#    #+#             */
-/*   Updated: 2024/11/05 14:05:30 by norabino         ###   ########.fr       */
+/*   Updated: 2024/11/05 14:12:52 by norabino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,22 @@
 
 void *ft_memcpy(void *dest, const void *src, size_t n)
 {
-    size_t  i;
-
-    i = 0;
-    if (src == 0 && n != 0)
-    {
-        if (dest == 0)
-            return (NULL);
-    }
-    while (i < n)
-    {
-        ((char *)dest)[i] = ((char *)src)[i];
-        i++;
-    }
+    char    *d;
+    char    *s;
+    
+    if (src == 0 && n != 0 && dest == 0)
+        return (NULL);
+    d = (char *)dest;
+    s = (char *)src;
+    while (n--)
+        d[n] = s[n];
     return (dest);
 }
 /*
 int main ()
 {
-   //printf("Og func = %s\n", memcpy("            ", ((void*)0), 17));
-   printf("My func = %s\n", ft_memcpy("            ", ((void*)0), 17));
+   printf("Og func = %s\n", memcpy((void*)0, (void*)0, 3));
+   printf("My func = %s\n", ft_memcpy((void*)0, (void*)0, 3));
 
    return(0);
 }*/
