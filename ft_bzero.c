a3087fd9d2fe73834a96f608d9980bbfe01c04c1
Noe Rabinovici <norabino@c2r4p7.42perpignan.fr>
2024-11-05 09:18:42 +0100

    bzero bon

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,11 +6,12 @@
 /*   By: norabino <norabino@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 17:28:51 by norabino          #+#    #+#             */
-/*   Updated: 2024/11/04 17:32:21 by norabino         ###   ########.fr       */
+/*   Updated: 2024/11/05 09:18:23 by norabino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <strings.h>
+#include <stdio.h>
 
 void    ft_bzero(void *s, size_t n)
 {
@@ -23,16 +24,9 @@ void    ft_bzero(void *s, size_t n)
         i++;
     }
 }
-
-#include <stdio.h>
+/*
 int main(void)
 {
     int tab[] = {54, 85, 20, 63, 21};
-    ft_bzero(tab, 5);
-    int i = 0;
-    while(tab[i])
-    {
-        printf("%d", tab[i]);
-        i++;
-    }
-}
+    ft_bzero(tab, 5);    
+}*/
