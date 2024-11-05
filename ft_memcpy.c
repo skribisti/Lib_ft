763575987ec24ea9454dd505468dad7eb391f017
Noe Rabinovici <norabino@c2r4p7.42perpignan.fr>
2024-11-05 13:53:42 +0100

    auto update

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: norabino <norabino@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 10:05:28 by norabino          #+#    #+#             */
-/*   Updated: 2024/11/05 13:45:37 by norabino         ###   ########.fr       */
+/*   Updated: 2024/11/05 13:53:38 by norabino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,12 @@ void *ft_memcpy(void *dest, const void *src, size_t n)
     size_t  i;
 
     i = 0;
+    if (src == 0)
+    {
+        if (dest == 0)
+            return (NULL);
+        return (dest);
+    }
     while (i < n)
     {
         ((char *)dest)[i] = ((char *)src)[i];
@@ -24,7 +30,7 @@ void *ft_memcpy(void *dest, const void *src, size_t n)
     }
     return (dest);
 }
-/*
+
 int main ()
 {
    const char *src = ((void *)0);
@@ -38,4 +44,4 @@ int main ()
    printf("My func = %s\n", dest);
 
    return(0);
-}*/
+}
