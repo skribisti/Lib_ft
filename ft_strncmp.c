a699b1beaf7beeb3312cb71a3475689103c24744
Noe Rabinovici <norabino@c2r8p1.42perpignan.fr>
2024-11-06 15:47:26 +0100

    auto update

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -19,15 +19,15 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	if (n == 0)
 		return (0);
-	while ((s1[i] || s2[i]) && (s1[i] == s2[i]) && (i < n - 1))
+	while (s1[i] && (s1[i] == s2[i]) && (i < n))
 		i++;
 	return (s1[i] - s2[i]);
 }
 /*
 int main()
 {
-    char *str1 = "\200";
-    char *str2 = "\0";
+    char *str1 = "\x12\xff\x65\x12\xbd\xde\xad";
+    char *str2 = "\x12\x02";
 	int	i = 0;
 
 	while (str1[i])
@@ -35,8 +35,8 @@ int main()
 		printf("%c\n", str1[i]);
 		i++;
 	}
-	printf("Og func :%d\n", strncmp(str1, str2, 1));
-    printf("My func :%d", ft_strncmp(str1, str2, 1));
+	printf("Og func :%d\n", strncmp(str1, str2, 2));
+    printf("My func :%d", ft_strncmp(str1, str2, 2));
     return 0;
 }
 */
