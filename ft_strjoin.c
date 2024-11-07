3e9f94c61eec392a56411c8c605a343bbd79aa0a
Noe Rabinovici <norabino@c2r5p9.42perpignan.fr>
2024-11-07 17:01:57 +0100

    auto update

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: norabino <norabino@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 16:56:01 by norabino          #+#    #+#             */
-/*   Updated: 2024/11/07 17:01:18 by norabino         ###   ########.fr       */
+/*   Updated: 2024/11/07 17:01:56 by norabino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ char *ft_strjoin(char const *s1, char const *s2)
     res = (char *)malloc(total_size);
     i = 1;
     while(s1[i++])
-        res[i] = s1[i]
+        res[i] = s1[i];
     j = i;
     while (s2[j++])
         res[j] = s2[j];
