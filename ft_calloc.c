c460d98e9fddecef0ac30ec35d6175b56647886a
Noe Rabinovici <norabino@c2r5p9.42perpignan.fr>
2024-11-07 15:49:25 +0100

    auto update

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: norabino <norabino@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 15:41:00 by norabino          #+#    #+#             */
-/*   Updated: 2024/11/07 15:49:00 by norabino         ###   ########.fr       */
+/*   Updated: 2024/11/07 15:49:23 by norabino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void    *ft_calloc(size_t nmemb, size_t size)
 {
     void    *ptr;
 
-    if (size == 0)
+    if (nmemb == 0)
         return (0);
     ptr = (void *)malloc(nmemb * size);
     if (!ptr)
