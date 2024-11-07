fab4cf51902e9d57728dc66fee13fcdb9c20a5fc
Noe Rabinovici <norabino@c2r5p8.42perpignan.fr>
2024-11-07 12:35:04 +0100

    auto update

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: norabino <norabino@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 11:39:32 by norabino          #+#    #+#             */
-/*   Updated: 2024/11/07 11:57:28 by norabino         ###   ########.fr       */
+/*   Updated: 2024/11/07 12:34:55 by norabino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,21 @@
 int        ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t	i;
-    char    *str1 = (char *)s1;
-    char    *str2 = (char *)s2;
 
 	i = 0;
-    if (n == 0)
-    {
-        return (0);
-    }
-	while ((str1[i] == str2[i]) && (str1[i]) && (str2[i]) && (i < n - 1))
+	while (*(unsigned char *)(s1 + i) == *(unsigned char *)(s2 + i) 
+    && (i < n))
 		i++;
-	return (str1[i] - str2[i]);
+    if (i < n)
+	    return (*(unsigned char *)(s1 + i) - *(unsigned char *)(s2 + i));
+    return (0);
 }
+/*
+int main(void)
+{
+    char    *s1 = "\xff\0\0\xaa\0\xde\xffMACOSX\xff";
+    char    *s2 = "\xff\0\0\xaa\0\xde\x00MBS";
+    size_t  n = 9;
+    printf("My func : %d\n", ft_memcmp(s1, s2, n));
+    return 0;
+}*/
