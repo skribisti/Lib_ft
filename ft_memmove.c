249c3a028ccd32a7a6b79247e689df88be9f6152
Noe Rabinovici <norabino@c2r3p8.42perpignan.fr>
2024-11-07 11:25:17 +0100

    auto update

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: norabino <norabino@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 11:11:14 by norabino          #+#    #+#             */
-/*   Updated: 2024/11/07 11:24:40 by norabino         ###   ########.fr       */
+/*   Updated: 2024/11/07 11:25:16 by norabino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,15 @@ void    *ft_memmove(void *dest, const void *src, size_t n)
     if (src > dest)
         return (ft_memcpy(dest, src, n));
     
-   /* char        *d;
-    char        *s;
-    size_t      i = 0;
+    char    *d;
+    char    *s;
     
     if (src == 0 && n != 0 && dest == 0)
         return (NULL);
     d = (char *)dest;
     s = (char *)src;
-    while (i < n)
-    {
-        d[i] = s[i];
-        i++;    
-    }*/
+    while (n--)
+        d[n] = s[n];
     return (dest);
     
 }
