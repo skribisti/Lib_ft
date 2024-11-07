904c739803812d72db285097de114d09b3e7143b
Noe Rabinovici <norabino@c2r5p8.42perpignan.fr>
2024-11-07 12:38:32 +0100

    auto update

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: norabino <norabino@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 11:39:32 by norabino          #+#    #+#             */
-/*   Updated: 2024/11/07 12:34:55 by norabino         ###   ########.fr       */
+/*   Updated: 2024/11/07 12:38:30 by norabino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,19 @@
 int        ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t	i;
+    unsigned char   *str1;
+    unsigned char   *str2;
 
+    str1 = (unsigned char *)s1;
+    str2 = (unsigned char *)s2;
 	i = 0;
-	while (*(unsigned char *)(s1 + i) == *(unsigned char *)(s2 + i) 
-    && (i < n))
-		i++;
-    if (i < n)
-	    return (*(unsigned char *)(s1 + i) - *(unsigned char *)(s2 + i));
+    while (i < n)
+    {
+        if (str1[i] != str2[i])
+            return (str1[i] - str2[i])
+        i++;
+    }
     return (0);
-}
 /*
 int main(void)
 {
