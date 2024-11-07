577fd7281efb236044732beda2dfe0398dab3c1c
Noe Rabinovici <norabino@c2r3p8.42perpignan.fr>
2024-11-07 11:38:14 +0100

    auto update

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: norabino <norabino@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 11:32:51 by norabino          #+#    #+#             */
-/*   Updated: 2024/11/07 11:37:14 by norabino         ###   ########.fr       */
+/*   Updated: 2024/11/07 11:38:12 by norabino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void    *ft_memchr(const void *s, int c, size_t n)
     while (i < n)
     {
         if (((char *)s)[i] == c)
-            return (s + i);
+            return (((char *)s) + i);
         i++;
     }
     return (NULL);
--- libft.h
+++ libft.h
@@ -39,7 +39,7 @@ void    ft_putstr_fd(char *s, int fd);
 void    ft_putendl_fd(char *s, int fd);
 void    *ft_memset(void *s, int c, size_t n);
 void    *ft_calloc(size_t nmemb, size_t size);
-void    ft_memchr(const void *s, int c, size_t n);
+void    *ft_memchr(const void *s, int c, size_t n);
 void    *ft_memcpy(void *dest, const void *src, size_t n);
 void    *ft_memmove(void *dest, const void *src, size_t n);
 
