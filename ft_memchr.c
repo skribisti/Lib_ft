5b06bb373e67265ff08b8dd60db014017a9b36f4
Noe Rabinovici <norabino@c2r3p8.42perpignan.fr>
2024-11-07 11:36:52 +0100

    auto update

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,13 +6,13 @@
 /*   By: norabino <norabino@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 11:32:51 by norabino          #+#    #+#             */
-/*   Updated: 2024/11/07 11:35:59 by norabino         ###   ########.fr       */
+/*   Updated: 2024/11/07 11:36:48 by norabino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    ft_memchr(const void *s, int c, size_t n)
+void    *ft_memchr(const void *s, int c, size_t n)
 {
     size_t  i = 0;
 
