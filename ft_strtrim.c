8fa5dd45ba93e11b1013076f5b2947176973badc
Noe Rabinovici <norabino@c2r5p9.42perpignan.fr>
2024-11-07 17:21:06 +0100

    auto update

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,33 +6,33 @@
 /*   By: norabino <norabino@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 17:09:03 by norabino          #+#    #+#             */
-/*   Updated: 2024/11/07 17:19:57 by norabino         ###   ########.fr       */
+/*   Updated: 2024/11/07 17:20:59 by norabino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t  ft_len(char const *s1, char const *set, size_t  length)
+size_t  ft_len(char const *s1, size_t  lset)
 {
-    return (ft_strlen(s1) - 2 * length);   
+    return (ft_strlen(s1) - 2 * lset);   
 }
 
 char *ft_strtrim(char const *s1, char const *set)
 {
     char    *res;
     size_t  total_len;
-    size_t  length;
+    size_t  lset;
     int     i;
 
-    length = ft_strlen(set);
-    total_len = ft_len(s1, set, length);
+    lset = ft_strlen(set);
+    total_len = ft_len(s1, lset);
     res = malloc(total_len);
     if (!res)
         return(NULL);
     i = 0;
     while (res[i])
     {
-        res[i] = s1[i + length];
+        res[i] = s1[i + lset];
         i++;
     }
     return (res);
