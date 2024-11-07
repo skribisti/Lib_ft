271dbc5b2904a272615e47bdf183967fc826f88b
Noe Rabinovici <norabino@c2r3p8.42perpignan.fr>
2024-11-07 11:04:25 +0100

    strnstr bon

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -27,13 +27,9 @@ char    *ft_strnstr(const char *big, const char *little, size_t len)
     while (big[i] && j < ft_strlen(little))
     {
         j = 0;
-        //printf("i = %d\n", (int)i);
         while (big[i + j] == little[j] && i < len
         && i + j < len)
         {
-            //printf("j = %d\n\n", (int)j);
-            //printf("big[i + j] = %c\n", (char)big[i + j]);
-            //printf("little[j] = %c\n\n\n", (char)little[j]);
             j++;
             if (j == ft_strlen(little))
                 return ((char *)big + i);
