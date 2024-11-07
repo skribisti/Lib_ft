6b4ae7a17885ef8bdbf1e44bb008d5968d82dc98
Noe Rabinovici <norabino@c2r5p8.42perpignan.fr>
2024-11-07 12:44:56 +0100

    auto update

---
+++ ft_strlcpy.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: norabino <norabino@student.42perpignan.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/07 12:42:42 by norabino          #+#    #+#             */
+/*   Updated: 2024/11/07 12:44:44 by norabino         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t    ft_strlcpy(char *dst, const char *src, size_t size);
+{
+	size_t	i;
+
+	i = 0;
+	while (src[i] && (i < size - 1))
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
