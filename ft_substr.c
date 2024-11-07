cbddf978d2f800de3cfb1180a27ac3298bd8d26a
Noe Rabinovici <norabino@c2r5p9.42perpignan.fr>
2024-11-07 16:26:15 +0100

    auto update

--- ft_substr.c
+++ ft_substr.c
@@ -6,10 +6,23 @@
 /*   By: norabino <norabino@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 16:20:52 by norabino          #+#    #+#             */
-/*   Updated: 2024/11/07 16:21:32 by norabino         ###   ########.fr       */
+/*   Updated: 2024/11/07 16:25:32 by norabino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 char    *ft_substr(char const *s, unsigned int start, size_t len)
+{
+    char    *str;
+    int     i;
+
+    str = malloc(len);
+    i = 0;
+    while (i < len)
+    {
+        str[i] = s[start + i];
+        i++;
+    }
+    return (str);
+}
