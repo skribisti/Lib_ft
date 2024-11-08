f9501a88ed76b3324a821986e3ec99e95a64821b
Noe Rabinovici <norabino@c2r5p9.42perpignan.fr>
2024-11-08 13:56:57 +0100

    auto update

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: norabino <norabino@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 11:32:22 by norabino          #+#    #+#             */
-/*   Updated: 2024/11/08 13:56:09 by norabino         ###   ########.fr       */
+/*   Updated: 2024/11/08 13:56:56 by norabino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,18 @@ size_t  ft_countsize(long nb)
     size_t  s;
     
     s = 0;
-    if (nb < 0)
+    if (nb <= 0)
     {
         nb = -nb;
         s++;
     }
-    while(nb >  9)
+    while(nb)
     {
         nb = nb / 10;
         s++;
     }
-    printf("s = %zu\n", s);
-    return (s + 1);
+    //printf("s = %zu\n", s);
+    return (s);
 }
 
 char *ft_itoa(int n)
@@ -38,19 +38,24 @@ char *ft_itoa(int n)
     long    nb;
 
     nb = (size_t)n;
-    size = 0;
-    str = (char *)malloc(sizeof(char) * (ft_countsize(nb) + 1));
+    size = ft_countsize(nb);
+    str = (char *)malloc(sizeof(char) * (size + 1));
     if (!str)
         return (NULL);
+    if (nb == 0)
+    {
+        str[0] = '0';
+        return (str);
+    }
     if (nb < 0)
     {
         str[0] = '-';
         nb = -nb;
     }
-    while(nb > 9)
+    str[size] = '\0';
+    while(nb)
     {
-        str[ft_countsize(nb) - size - 1] = nb % 10 + '0';
-        size++;
+        str[--size - 1] = nb % 10 + '0';
         nb = nb / 10;
     }
     return (str);
