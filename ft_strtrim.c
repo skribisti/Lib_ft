35eb259fc0541d8ea3a4973e969f22ee35bbcfc3
Noe Rabinovici <norabino@c2r5p9.42perpignan.fr>
2024-11-08 14:25:04 +0100

    auto update

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,27 +6,42 @@
 /*   By: norabino <norabino@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 17:09:03 by norabino          #+#    #+#             */
-/*   Updated: 2024/11/08 14:16:28 by norabino         ###   ########.fr       */
+/*   Updated: 2024/11/08 14:24:59 by norabino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-/*
-char *ft_strtrim(char const *s1, char const *set)
+
+int ft_incharset(int c, char *set)
 {
-    
+    int i;
+
+    while(set[i])
+    {
+        if (c == set[i])
+            return (1);
+        i++;
+    }
+    return (0);
 }
 
+char *ft_strtrim(char const *s1, char const *set)
+{
+    int i;
+    int j;
 
+    i = -1;
+    j = ft_strlen(s1);
+    
+    while(s1[++i] && ft_ischarset(s1[i], set));
+    while(s1[--j] && ft_incharset(s1[j], set));
+    return (ft_substr(s1, i , j - i));  
+}
+
+/*
 int main(void)
 {
     char    *str1 = "Hello   Please Trim me !H";
     printf("%s", ft_strtrim(str1, "H"));
     return (0);
-
-    ' b'
-
-    '     bonjo ur   '
-
-    'onjo ur'
 }*/
