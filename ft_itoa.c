8e20a01b046255095739354960458d2b5c614e25
Noe Rabinovici <norabino@c2r5p9.42perpignan.fr>
2024-11-08 12:33:01 +0100

    auto update

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: norabino <norabino@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 11:32:22 by norabino          #+#    #+#             */
-/*   Updated: 2024/11/08 11:50:49 by norabino         ###   ########.fr       */
+/*   Updated: 2024/11/08 12:32:58 by norabino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,19 @@
 size_t  ft_countsize(long nb)
 {
     size_t  s;
-
-    if (nb < 0)
-        nb = -nb;
-    if (nb == 0)
-        s = 0;
-    else
+    
+    s = 0;
+    if (nb <= 0)
     {
-        while(nb)
-        {
-            nb = nb / 10;
-            s++;
-        }
+        nb = -nb;
+        s++;
     }
+    while(nb)
+    {
+        nb = nb / 10;
+        s++;
+    }
+    printf("s = %zu\n", s);
     return (s);
 }
 
@@ -35,32 +35,34 @@ char *ft_itoa(int n)
 {
     char    *str;
     size_t  size;
-    size_t  is_neg;
-    int     i;
-    int     end;
+    long    nb;
 
-    size = ft_countsize((long)n);
+    nb = (size_t)n;
+    size = ft_countsize(nb);
     str = (char *)malloc(size + 1);
+    if (!nb)
+    {
+        str[0] = '0';
+        return (str);
+    }
     if (!str)
         return (NULL);
-    is_neg = 0;
-    if (n < 0)
+    if (nb < 0)
     {
         str[0] = '-';
-        n = -n;
-        is_neg = 1;
+        nb = -nb;
     }
-    str[size] = 0;
-    if (is_neg)
-        end = 1;
-    else
-        end = 0;
-    i = 0;
-    while(i > end)
+    str[size] = '\0';
+    while(nb)
     {
-        str[size] = n % 10 + '0';
-        n = n / 10;
-        i--;
+        str[--size] = nb % 10 + '0';
+        nb = nb / 10;
     }
     return (str);
 }
+/*
+int main(void)
+{
+    int a = -5859; //156 -0 2147483647 -2147483648 -1234 -5859
+    printf("My func : %s", ft_itoa(a));
+}*/
