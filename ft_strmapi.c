400978daf516d53bd6f4a024217c67b0f7848b43
Noe Rabinovici <norabino@c2r5p9.42perpignan.fr>
2024-11-08 13:46:21 +0100

    auto update

---
+++ ft_strmapi.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: norabino <norabino@student.42perpignan.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/08 13:33:20 by norabino          #+#    #+#             */
+/*   Updated: 2024/11/08 13:40:56 by norabino         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+    char    *res;
+    
+    res = (char *)malloc(ft_strlen(s) + 1);
+    if(!res)
+    
+}
