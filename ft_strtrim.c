acc237d6c56a52d0bd82b1f34ae6780f03877bd8
Noe Rabinovici <norabino@c2r5p9.42perpignan.fr>
2024-11-08 14:31:59 +0100

    auto update

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: norabino <norabino@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 17:09:03 by norabino          #+#    #+#             */
-/*   Updated: 2024/11/08 14:30:36 by norabino         ###   ########.fr       */
+/*   Updated: 2024/11/08 14:31:56 by norabino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,8 +34,10 @@ char *ft_strtrim(char const *s1, char const *set)
     i = -1;
     j = ft_strlen(s1);
     
-    while(s1[++i] && ft_incharset(s1[i], (char *)set) == 1);
-    while(s1[--j] && ft_incharset(s1[j], (char *)set) == 1);
+    while(s1[++i] && ft_incharset(s1[i], (char *)set) == 1)
+        ;
+    while(s1[--j] && ft_incharset(s1[j], (char *)set) == 1)
+        ;
     return (ft_substr(s1, i , j - i + 1));  
 }
 
