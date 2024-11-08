fbcc98adfb89b14162db2987a0bc6a3e2165491b
Noe Rabinovici <norabino@c2r5p9.42perpignan.fr>
2024-11-08 15:14:50 +0100

    auto update

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: norabino <norabino@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 15:09:44 by norabino          #+#    #+#             */
-/*   Updated: 2024/11/08 15:14:13 by norabino         ###   ########.fr       */
+/*   Updated: 2024/11/08 15:14:50 by norabino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ void ft_putnbr_fd(int n, int fd)
     z = '0';
     if (n < 0)
     {
-        n = - n;
+        n = -n;
         ft_putchar_fd('-', fd);
     }
     if (n > 9)
         ft_putnbr_fd(n / 10, fd);
-    z = z + n % 10 + '0';
+    z = z + n % 10;
     ft_putchar_fd(z, fd);
 }
