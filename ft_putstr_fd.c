0db53405517274cedf81a0f5812e114058734e5c
Noe Rabinovici <norabino@c2r5p9.42perpignan.fr>
2024-11-08 15:01:42 +0100

    auto update

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: norabino <norabino@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 14:58:30 by norabino          #+#    #+#             */
-/*   Updated: 2024/11/08 15:00:51 by norabino         ###   ########.fr       */
+/*   Updated: 2024/11/08 15:01:41 by norabino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@ void ft_putstr_fd(char *s, int fd)
 {
     int i;
     
+    if (!s)
+        return ;
     i = -1;
     while(s[++i])
-        ft_putchar_fd(fd, &s[i]);
+        ft_putchar_fd(s[i], fd);
 }
