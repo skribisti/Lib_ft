1d9b6e7034be35bf18c27b7d7dbae23502922c2a
Noe Rabinovici <norabino@c2r5p9.42perpignan.fr>
2024-11-08 12:43:27 +0100

    auto update

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,12 +6,64 @@
 /*   By: norabino <norabino@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 11:32:22 by norabino          #+#    #+#             */
-/*   Updated: 2024/11/08 12:41:08 by norabino         ###   ########.fr       */
+/*   Updated: 2024/11/08 12:43:26 by norabino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static size_t	count_size(long nb)
+{
+	size_t	size;
+
+	size = 0;
+	if (nb < 0)
+	{
+		nb = nb * (-1);
+		size = 1;
+	}
+	if (nb == 0)
+		size = 1;
+	else
+	{
+		while (nb)
+		{
+			nb = nb / 10;
+			size++;
+		}
+	}
+	return (size);
+}
+
+char	*ft_itoa(int n)
+{
+	size_t	size;
+	long	nb;
+	char	*str;
+	int		is_negative;
+
+	size = count_size((long) n);
+	str = (char *) malloc(sizeof(char) * (size + 1));
+	if (str == NULL)
+		return (NULL);
+	nb = (long) n;
+	is_negative = 0;
+	if (nb < 0)
+	{
+		nb = nb * (-1);
+		str[0] = '-';
+		is_negative = 1;
+	}
+	str[size] = '\0';
+	while (size > (size_t) is_negative)
+	{
+		str[size - 1] = nb % 10 + '0';
+		nb = nb / 10;
+		size--;
+	}
+	return (str);
+}
+/*
 size_t  ft_countsize(long nb)
 {
     size_t  s;
@@ -27,7 +79,7 @@ size_t  ft_countsize(long nb)
         nb = nb / 10;
         s++;
     }
-    //printf("s = %zu\n", s);
+    printf("s = %zu\n", s);
     return (s);
 }
 
@@ -63,6 +115,6 @@ char *ft_itoa(int n)
 
 int main(void)
 {
-    int a = 1000526;
+    int a = 0;
     printf("My func : %s", ft_itoa(a));
-}
+}*/
