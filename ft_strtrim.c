364b5d31a90ef50ea93c1ee770948b5216395bb2
Noe Rabinovici <norabino@c2r5p9.42perpignan.fr>
2024-11-08 14:17:00 +0100

    auto update

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,42 +6,27 @@
 /*   By: norabino <norabino@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 17:09:03 by norabino          #+#    #+#             */
-/*   Updated: 2024/11/08 09:16:05 by norabino         ###   ########.fr       */
+/*   Updated: 2024/11/08 14:16:28 by norabino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-size_t  ft_len(char const *s1, size_t  lset)
-{
-    return (ft_strlen(s1) - 2 * lset);   
-}
-
+/*
 char *ft_strtrim(char const *s1, char const *set)
 {
-    char    *res;
-    size_t  total_len;
-    size_t  lset;
-    int     i;
-
-    lset = ft_strlen(set);
-    total_len = ft_len(s1, lset) + 1;
-    res = malloc(total_len);
-    if (!res)
-        return(NULL);
-    i = 0;
-    while (res[i])
-    {
-        res[i] = s1[i + lset];
-        i++;
-    }
-    res[i] = 0;
-    return (res);
+    
 }
-/*
+
+
 int main(void)
 {
     char    *str1 = "Hello   Please Trim me !H";
     printf("%s", ft_strtrim(str1, "H"));
     return (0);
+
+    ' b'
+
+    '     bonjo ur   '
+
+    'onjo ur'
 }*/
