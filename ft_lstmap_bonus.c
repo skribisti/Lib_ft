1775b0652dfb8eb8fa1ce2445ed5e1ef0411f775
Noe Rabinovici <norabino@c2r3p8.42perpignan.fr>
2024-11-13 11:18:53 +0100

    libft 125% (sauf 2erreurs leaks substr)

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,13 +6,28 @@
 /*   By: norabino <norabino@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 10:02:24 by norabino          #+#    #+#             */
-/*   Updated: 2024/11/13 10:02:49 by norabino         ###   ########.fr       */
+/*   Updated: 2024/11/13 10:21:00 by norabino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	
+	t_list	*new;
+	t_list	*temp;
+
+	if (!lst || !f || !del)
+		return (NULL);
+	new = ft_lstnew(f(lst->content));
+	temp = new;
+	while (lst && lst->next)
+	{
+		if (!temp)
+			return (NULL);
+		temp->next = ft_lstnew(f(lst->next->content));
+		temp = temp->next;
+		lst = lst->next;
+	}
+	return (new);
 }
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: norabino <norabino@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 16:20:52 by norabino          #+#    #+#             */
-/*   Updated: 2024/11/12 15:28:43 by norabino         ###   ########.fr       */
+/*   Updated: 2024/11/13 11:15:54 by norabino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,24 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
+	int		i;
 	char	*str;
-	size_t	i;
 
 	if (!s)
 		return (NULL);
 	if (start >= ft_strlen(s))
-		len = 0;
-	str = malloc(len + 1);
+	{
+		str = (char *)malloc(sizeof(char));
+		if (!str)
+			return (NULL);
+		str[0] = '\0';
+		return (str);
+	}
+	str = (char *)malloc(len + 1);
 	if (!str)
 		return (NULL);
 	i = 0;
-	while (i < len && s[i])
+	while (i < (int)len && s[i])
 	{
 		str[i] = s[start + i];
 		i++;
@@ -33,6 +39,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	str[i] = 0;
 	return (str);
 }
+
 /*
 int main(void)
 {
